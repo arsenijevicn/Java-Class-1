b78dc99154c56fb7d80293c6e257cde5b2904600
Nebojsa Arsenijevic <arsenijevicn@gmail.com>
2018-01-03 21:43:37 +0100

    Example 2: Examples, part 1

---
+++ src/main/java/com/javaclass/first/exercises/bookExercise/LexicalStructure.java
@@ -0,0 +1,58 @@
+package com.javaclass.first.exercises.bookExercise;
+
+public class LexicalStructure {
+    // 1.Comments
+
+    // one line comments
+
+    /*
+    * multiple
+    * line
+    * comments
+    */
+
+    /**
+     * Document comment
+     * multiple
+     * line
+     */
+
+    //2.Identifers
+
+    /*legal identifiers:
+    i
+    x
+    yz
+    asd123
+    theSmallBuilding
+    dark_lobby
+
+    * */
+
+    //3.Literals
+
+    /*
+    15
+    1235.35465465
+    "Peter Pan"
+    false
+    null
+     */
+
+    //4.Punctuation
+
+        //4.1.Separators
+
+        /*
+        * ( ) { } [ ]
+        ... @ ::
+        ; , .
+        * */
+
+        //4.2.Operators
+
+
+
+
+
+}
---
+++ src/main/java/com/javaclass/first/exercises/bookExercise/PrimitiveDataTypes.java
@@ -0,0 +1,6 @@
+package com.javaclass.first.exercises.bookExercise;
+
+public class PrimitiveDataTypes {
+    //boolean, char, byte, short, int, long, float, double
+
+}
